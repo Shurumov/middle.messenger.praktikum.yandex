f99b1fb246e468e1ae052f0b00ae388056fec5ed
Shurumov <aleksey.shurumov@aliexpress.ru>
2022-01-25 14:48:34 +0300

    render sign in on client

--- pages/index.ts
+++ pages/index.ts
@@ -1,7 +1,9 @@
-import { Block } from '@/utils/block';
-
+import templateFunction from './index.hbs';
 
 function render() {
+  if(document?.querySelector("#root")?.innerHTML){
+    document?.querySelector("#root")?.innerHTML = templateFunction();
+  }
 }
 
 render();
