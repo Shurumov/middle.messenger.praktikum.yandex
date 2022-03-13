1e683078fbb513aded8d89b86bd38031ef78401e
Shurumov <aleksey.shurumov@aliexpress.ru>
2022-03-13 16:03:23 +0300

    change port

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.static('./dist'));
 
@@ -10,6 +9,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.listen(3000, () => {
+  console.log(`Example app listening on port ${3000}!`);
 });
